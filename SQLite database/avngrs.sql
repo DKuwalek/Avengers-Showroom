cfae764a955d1de489843e7b417afe07701e7494
dkuwalek <d.kuwalek@gmail.com>
2019-05-22 22:43:25 +0200

    Fix for sql
    Fixed the Date format for data which is automatically generated in the database tables

--- SQLite database/avngrs.sql
+++ SQLite database/avngrs.sql
@@ -18,8 +18,8 @@ CREATE TABLE
 	);
 
 INSERT INTO avngr (id, name, number_of_movies, friendly, last_movie_release_date)
-	VALUES (1, "Iron Man", 11, true, "2019-01-01");
+	VALUES (1, "Iron Man", 11, true, "2019-01-01 00:00:00.00");
 INSERT INTO avngr (id, name, number_of_movies, friendly, last_movie_release_date)
-	VALUES (2, "Thor", 3, true, "2019-01-01");
+	VALUES (2, "Thor", 3, true, "2019-01-01 00:00:00.00");
 
 INSERT INTO hibernate_sequence (next_value) VALUES (3);
